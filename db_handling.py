e9d2ae9740fe289b6e36ec4ca621bb5c19be496c
spitihnev <peter.lacikaa@gmail.com>
2020-05-25 19:25:25 +0200

    a little more work on DB

--- db_handling.py
+++ db_handling.py
@@ -1,26 +1,50 @@
-
 import pymysql
 import logging
 import traceback
 
 import config
+import constants
 
 LOG = logging.getLogger(__name__)
 
 class DatabaseError(Exception):
     pass
-
+class UserAlreadyExists(Exception):
+    pass
 class UserNotFoundError(Exception):
     pass
+class RealmAlreadyExists(Exception):
+    pass
+class UnknownRealmName(Exception):
+    pass
 
-def add_tranaction(type, booster_name, amount, comment=None):
-    raise NotImplementedError
+def add_tranaction(type, booster_name, amount, realm_name, comment=None):
+    booster_id = name2id(booster_name)
+    realm_id = realm_name2id(realm_name)
+    with _db_connect() as conn:
+        crs = conn.cursor()
+        try:
+            crs.execute('insert into transactions ("type", "booster_id", "amount", "comment")', (type, booster_id, amount, comment))
+        except:
+            raise DatabaseError(f'Failed to add transaction with parameters {type} {booster_name} (translated into {booster_id}) {realm_name} (translated into {realm_id}) {amount} {comment}, reason: {traceback.format_exc()}')
 
 def add_boost(raw_boostee, advertisers, boosters, price, comment):
     raise NotImplementedError
 
+def get_realm_balance(realm_name):
+    raise NotImplementedError
+
 def add_user(name, discord_id):
+    exist_check = None
+    try:
+        exist_check = name2id(name)
+    except UserNotFoundError:
+        pass
+    if exist_check:
+        raise UserAlreadyExists(f'User with name {name} already exists')
+
     with _db_connect() as conn:
+        crs = conn.cursor()
         try:
             crs.execute('insert into users ("name", "dsc_id") values ("%s", "%s")', (name, discord_id))
             conn.commit()
@@ -28,6 +52,9 @@ def add_user(name, discord_id):
             raise DatabaseError(f'Failed to add new realm with name {name}: {traceback.format_exc()}')
 
 def add_realm(name):
+    if name not in constants.EU_REALM_NAMES:
+        raise UnknownRealmName(f'Realm {name} is not a known EU realm.')
+
     with _db_connect() as conn:
         try:
             crs.execute('insert into realms ("name") values ("%s")', name)
